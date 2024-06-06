d07b12367df454ca18d55b39f1e219ed9ced615a
IAMOTAS <niiotasalokome@gmail.com>
2024-06-06 04:37:26 +0000

    6th-june-2024

--- React-Weather-App/src/components/Weather.jsx
+++ React-Weather-App/src/components/Weather.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import './Weather.css';
+import clear_icon from '../assets/clear.png';
+import clouds_icon from '../assets/search.png';
+import drizzle_icon from '../assets/drizzle.png';
+import humidity_icon from '../assets/humidity.png';
+import mist_icon from '../assets/mist.png';
+import rain_icon from '../assets/rain.png';
 import search_icon from '../assets/search.png';
+import snow_icon from '../assets/snow.png';
+import wind from '../assets/wind.png';
 
 
 
@@ -14,6 +22,9 @@ const Weather = () => {
         <input type='text' placeholder = 'Search'/>
         <img src={search_icon} alt=''/>
     </div>
+    <img src={clear_icon} alt='' className='weather-icon'/>
+    <p className='temperature'>16°c</p>
+    <p className='location'>London</p>
 
     </div>
   )
