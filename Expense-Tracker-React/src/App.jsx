0e4da29e8aea1e6cd5ac37c05f98812035be230c
IAMOTAS <niiotasalokome@gmail.com>
2024-06-08 19:01:17 +0000

    8-june-2024

--- Expense-Tracker-React/src/App.jsx
+++ Expense-Tracker-React/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
 import Header from './components/Header.jsx';
+import Balance from './components/Balance.jsx';
 function App() {
   
 
   return (
     <>
       <div>
+
       <Header/>
+
+      <div className='container'>
+      <Balance/>
+      </div>
+
       </div>
    
     </>
