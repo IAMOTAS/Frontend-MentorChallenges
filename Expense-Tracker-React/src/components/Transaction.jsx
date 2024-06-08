cb87f46872f6580e6be03abc70520764199d5bab
IAMOTAS <niiotasalokome@gmail.com>
2024-06-08 23:15:46 +0000

    8-june-2024

--- Expense-Tracker-React/src/components/Transaction.jsx
+++ Expense-Tracker-React/src/components/Transaction.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Transaction = () => {
+const Transaction = ({transaction}) => {
   return (
     
       <li className='minus'>
--- Expense-Tracker-React/src/components/TransactionList.jsx
+++ Expense-Tracker-React/src/components/TransactionList.jsx
@@ -1,4 +1,5 @@
 import React,{useContext} from 'react';
+import Transaction from './Transaction';
 import GlobalContext from '../context/GlobalState';
 
 const TransactionList = () => {
@@ -11,7 +12,7 @@ const TransactionList = () => {
       
     <h3>History</h3>
     <ul className='list'>
-      {transactions.map(transaction => ())}
+      {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction}/>))}
         
     </ul>
 
