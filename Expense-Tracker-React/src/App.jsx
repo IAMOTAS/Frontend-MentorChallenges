5d9f578f2c18568320cc593f5f2c340e7bf807b9
IAMOTAS <niiotasalokome@gmail.com>
2024-06-08 20:27:19 +0000

    8th-june-2024

--- Expense-Tracker-React/src/App.jsx
+++ Expense-Tracker-React/src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import Header from './components/Header.jsx';
 import Balance from './components/Balance.jsx';
+import IncomeExpenses from './components/IncomeExpenses.jsx';
+import TransactionList from './components/TransactionList.jsx';
+import AddTransaction from './components/AddTransaction.jsx';
 function App() {
   
 
@@ -12,6 +15,9 @@ function App() {
 
       <div className='container'>
       <Balance/>
+      <IncomeExpenses/>
+      <TransactionList/>
+      <AddTransaction/>
       </div>
 
       </div>
