5d881201cc7b3f211e4788fee69f07736101300e
IAMOTAS <niiotasalokome@gmail.com>
2024-06-09 01:22:34 +0000

    final-commit

--- Expense-Tracker-React/src/context/GlobalState.jsx
+++ Expense-Tracker-React/src/context/GlobalState.jsx
@@ -5,10 +5,7 @@ import AppReducer from './AppReducer';
 
 const initialState = {
     transactions: [
-        {id:1,text:'Flower',amount:-20},
-        {id:2,text:'Salary',amount:300},
-        {id:3,text:'Book',amount:-10},
-        {id:4,text:'Camera',amount:150}
+        
     ]
 }
 
@@ -26,13 +23,21 @@ function deleteTransaction(id){
     dispatch({
         type:'DELETE_TRANSACTION',
         payload: id
-    })
+    });
+}
+
+function addTransaction(transaction){
+    dispatch({
+        type:'ADD_TRANSACTION',
+        payload: transaction
+    });
 }
 
 
     return(<GlobalContext.Provider value={{
         transactions: state.transactions,
-        deleteTransaction
+        deleteTransaction,
+        addTransaction
     }}>
         {children}
     </GlobalContext.Provider>);
