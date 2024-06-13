9d322aace48388e71b686ae858dcf0c64f03cf12
IAMOTAS <niiotasalokome@gmail.com>
2024-06-13 22:01:20 +0000

    13th-june-2024

--- learn vue in 3days/complete-prototype/vue-test/src/components/AboutUs.vue
+++ learn vue in 3days/complete-prototype/vue-test/src/components/AboutUs.vue
@@ -7,12 +7,28 @@
                 <img src="" alt="_blank"/>
             </v-col>
     
-            <V-col>
+            <V-col lg="6">
                 <V-subheader class="text-h4">
                     About Us
                 </V-subheader>
+                <p>This company was established in the year 2023.</p>
             </V-col>
     
+            <v-list>
+                <v-list-item>
+                    <v-icon color="success">md-check-all</v-icon>
+                    <v-list-item-subtitle> we offer wide range of I.T Services</v-list-item-subtitle>
+                </v-list-item>
+                <v-list-item>
+                    <v-icon color="success">md-check-all</v-icon>
+                    <v-list-item-subtitle> Talk about Artificial Intelligence, Machine Learning, Quantum Programming, Crytoprogramming, webservices,mobile,electronics,telecomunications,networking,systems Administration e.t.c</v-list-item-subtitle>
+                </v-list-item>
+                <v-list-item>
+                    <v-icon color="success">md-check-all</v-icon>
+                    <v-list-item-subtitle> Your Business is in Safe Hands.</v-list-item-subtitle>
+                </v-list-item>
+                <p>We are here to partner with you.The Best Customer Service you can ever dream about. we have the solutions. we solve your problems in less than 24 hours.</p>
+            </v-list>
     
     </v-row>
     
