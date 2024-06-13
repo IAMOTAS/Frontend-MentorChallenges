38fa0c3ad865e6109a24002447c63b34ebfc78ee
IAMOTAS <niiotasalokome@gmail.com>
2024-06-13 20:10:53 +0000

    13th-june-2024

---
+++ learn vue in 3days/complete-prototype/vue-test/src/components/AppHeader.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-app-bar
+      style="padding:0px 90px"
+      app
+      color="dark"
+      light
+      elevation="6"
+      elevate-on-scroll
+    >
+      <v-toolbar-title>O.T.A.S</v-toolbar-title>
+      <v-spacer />
+      <v-list class="d-flex align-center">
+        <v-list-item v-for="(menu, index) in menus" :key="index" :to="menu.route">
+          <v-list-item-title>{{ menu.title }}</v-list-item-title>
+        </v-list-item>
+  
+        <v-btn text>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+  
+        <v-btn outlined color="success">Get Started</v-btn>
+      </v-list>
+    </v-app-bar>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AppHeader',
+    data() {
+      return {
+        menus: [
+          { title: 'Home', route: 'home' },
+          { title: 'About', route: 'about' },
+          { title: 'Services', route: 'services' },
+          { title: 'Support', route: 'support' },
+          { title: 'Vision', route: 'vision' },
+          { title: 'Mission', route: 'mission' },
+          { title: 'Contact', route: 'contact' },
+          { title: 'Partners', route: 'partners' },
+          { title: 'Languages', route: 'languages' },
+        ],
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Your styles here */
+  </style>
+  
