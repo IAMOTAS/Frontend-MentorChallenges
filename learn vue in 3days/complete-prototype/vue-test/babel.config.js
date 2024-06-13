5c233339fcab71a54f658df426bc6e4877b1beaf
IAMOTAS <niiotasalokome@gmail.com>
2024-06-13 19:26:43 +0000

    13th-june-2024

--- learn vue in 3days/complete-prototype/vue-test/babel.config.js
+++ learn vue in 3days/complete-prototype/vue-test/babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    requireConfigFile: false // Disables the requirement for a Babel config file
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  }
+};
