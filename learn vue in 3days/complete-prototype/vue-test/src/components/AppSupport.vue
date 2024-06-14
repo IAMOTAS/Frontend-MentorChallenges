0fddbed8b28d5450a47c55f5c42f16ce51916fd5
IAMOTAS <niiotasalokome@gmail.com>
2024-06-14 15:49:45 +0000

    14th-june-2024

---
+++ learn vue in 3days/complete-prototype/vue-test/src/components/AppSupport.vue
@@ -0,0 +1,28 @@
+<template>
+
+<v-container class="my-15">
+    
+    <V-row justify="center">
+
+        <v-col lg="12" class="text-center">
+            <v-subheader class="text-h4 justify-center">
+                Support
+            </v-subheader>
+            <p>Below are the services we offer and some already completed Projects.</p>
+        </v-col>
+        </V-row>
+    </v-container>
+
+</template>
+
+<script>
+
+export default{
+    name:"AppSupport"
+}
+
+</script>
+
+<style>
+
+</style>
