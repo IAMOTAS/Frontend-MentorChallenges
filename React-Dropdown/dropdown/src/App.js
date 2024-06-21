9965b8fe60516e19b62c2d25845516bed084b889
IAMOTAS <niiotasalokome@gmail.com>
2024-06-21 22:51:31 +0000

    21-june-2024

--- React-Dropdown/dropdown/src/App.js
+++ React-Dropdown/dropdown/src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
+import Dropdown from './Components/Dropdown/Dropdown';
 
 function App() {
+
+      const items = [1, 2, 3, 4, 5, 6, 7, 8];
+
   return (
     <div className="App">
-      
+     
+        <div className='content'>
+        <Dropdown buttonText = 'Dropdown button' content ={<p>Hello world</p>}/>
+        </div>
+
     </div>
   );
 }
---
+++ React-Dropdown/dropdown/src/Components/DropdownContent/DropdownContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DropdownContent = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default DropdownContent;
