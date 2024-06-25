155874e34251692e4b4284d869276d65fdabf7e8
IAMOTAS <niiotasalokome@gmail.com>
2024-06-25 13:11:31 +0000

    25-june-2024

---
+++ learn vue in 3days/2821labs.com/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app>
+
+    <v-app-bar
+
+style="padding:0px 90px"
+
+ app
+ color="dark"
+ light
+ elevation="6"
+ elevate-on-scroll
+ 
+ >
+
+ <v-toolbar-title>2821labs(Logo)</v-toolbar-title>
+<v-spacer/>
+ <v-list class="d-flex align-center">
+
+<v-list-item  v-for="(menu, index) in menus" :key="index" :to="menu.route">
+  <v-list-item-title>{{ menu.title }}</v-list-item-title>
+</v-list-item>
+
+ </v-list>
+
+</v-app-bar>
+
+ <v-main>
+   
+  
+ </v-main>
+
+
+  </v-app>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+data(){
+  return{
+    menus: [
+      {title:'Home',     route:'home'},
+      {title:'About',    route:'about'},
+      {title:'Services', route:'services'},
+      {title:'Support',  route:'support'},
+      {title:'Vision',   route:'vision'},
+      {title:'Mission',  route:'mission'},
+      {title:'Contact',  route:'contact'},
+      {title:'Partners', route:'partners'},
+      {title:'Languages',route:'Languages'},
+    ]
+  }
+}
+
+}
+</script>
